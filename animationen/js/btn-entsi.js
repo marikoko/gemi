c2860f15c385afd428325a45521ea0bfd3ac0ebb
mariko <heikoko166@gmail.com>
2019-08-15 21:52:31 +0200

    entsiBtnAni

--- animationen/js/btn-entsi.js
+++ animationen/js/btn-entsi.js
@@ -1,25 +1,26 @@
 console.log('hallo');
 
+const entsiBtnAni = document.querySelector('#entsiBtn-ani')
+      entsiBtnText = document.querySelector('#entsiBtn-text'),
+      buttons = [entsiBtnAni, entsiBtnText];
+      btn = document.querySelector("#entsiBtn");
+
 animateButton = (e) => {
     // console.log('geklickt');
-
-    // btn.classList.toggle('animate');
     e.preventDefault;
 
+    changeColor();
+
     //reset animation
-    const entsiBtnAni = document.querySelector('#entsiBtn-ani');
     entsiBtnAni.classList.remove('animate');
-    
-    console.log('entfernt');
+    // console.log('entfernt');
+
     entsiBtnAni.classList.add('animate');
-    console.log('dazu');
+    // console.log('dazu');
     
     // e.target.classList.add('animate');
     // console.log('dazu2');
-    setTimeout(function(){
-      entsiBtnAni.classList.remove('animate');
-      console.log('entfernt nach time');
-    },6000);
+    removeAll();
 
     //reset animation
     // e.target.classList.remove('animate');
@@ -36,12 +37,26 @@ animateButton = (e) => {
     // },6000);
   };
 
-const btn = document.querySelector("#entsiBtn");
+changeColor = () => {
+  buttons.forEach((i) => {
+    i.style.backgroundColor = '#5a5a5a';
+  })
+  entsiBtnText.style.color = "rgba(255,255,255,0.6)";
+}
+
+animationEnd = () => {
+  buttons.forEach((i) => {
+    i.style.backgroundColor = '#ffffff';
+    i.style.color = '#000';
+  })
+}
+
+removeAll = () => {
+  setTimeout(function(){
+    entsiBtnAni.classList.remove('animate');
+    animationEnd();
+    console.log('entfernt nach time');
+  },6000);
+}
 
 btn.addEventListener('click', animateButton, false);
-
-
-// const btnC = document.querySelector("#btn-C"),
-//     btnTextC = document.querySelector('#btn-text-C');
-
-// btnC.addEventListener('click', animateButton, false);
